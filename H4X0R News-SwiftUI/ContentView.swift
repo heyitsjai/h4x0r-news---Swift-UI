ba47309aa862504faca39b2b8c65d214a224e256
Jai Agrawal <jagrawa2@illinois.edu>
2019-11-27 10:28:54 -0800

    Initial Commit

---
+++ H4X0R News-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4X0R News-SwiftUI
+//
+//  Created by Jai Agrawal on 11/27/19.
+//  Copyright © 2019 Jai Agrawal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
